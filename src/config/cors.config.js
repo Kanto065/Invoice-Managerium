ab795a4264eafe41cf276a50a034589a29ccfbb1
kanto065 <kanto.uv@outlook.com>
2026-04-16 03:32:54 +0600

    added cors

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:4000",
+  "http://localhost:5173",
   // Add any other origins you need to allow
 ];
 
